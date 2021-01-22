aa875e97691f6b1f72c25a9508a7e17df22e2d17
mnycz <mnycz04@gmail.com>
2021-01-21 19:05:31 -0500

    Changes array Deck[] to be type ArrayList<Card>, to allow for mutable list.

--- src/Deck.java
+++ src/Deck.java
@@ -1,20 +1,29 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Deck {
-    private Card[] deck;
+    // private Card[] deck;
+    ArrayList<Card> deck = new ArrayList<>();
 
     public void init() {
-        deck = new Card[52];
 
         for (int i = 0; i < 52; i++) {
             Card card;
             card = new Card();
             card.set_card_ID(i);
             card.init_cardName();
-            deck[i] = card;
+            deck.add(card);
         }
     }
 
-    public Card[] get_deck() {
+    public ArrayList<Card> get_deck() {
         return deck;
     }
 
+    public Card draw_card() {
+        int card_ID = ThreadLocalRandom.current().nextInt(0, deck.size() + 1);
+        Card card = deck.remove(card_ID);
+
+        return card;
+    }
 }
